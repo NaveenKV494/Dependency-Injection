3657dbd995304ee7376386537586691387219bbb
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-20 19:47:30 +0530

    Created class "Interfaces"

---
+++ Dependency Injection/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection
+{
+    public class Interfaces
+    {
+        public interface ITransformer
+        {
+            void Attack();
+        }
+
+        public interface IGun
+        {
+            void Shoot();
+        }
+
+        public interface ISword
+        {
+            void Slash();
+        }
+
+    }
+}
